da18b6a44c552fcf8fdcfe3948ef07d20238b691
purpleow1 <purpleowllight@gmail.com>
2018-01-12 21:34:01 +0300

    google-page/fix: fix changing of search box shadow

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-inputid.onclick = function () {
-        boxid.style.boxShadow = "0 3px 6px 0 rgba(0, 0, 0, 0.16)";
-      };
-      inputid.onblur = function () {
-        boxid.style.boxShadow = "0 1px 1px 0 rgba(0, 0, 0, 0.16)";
-      };
+var flag = false;
+inputid.onfocus = function () {
+	boxid.style.boxShadow = "0 3px 6px 0 rgba(0, 0, 0, 0.16)";
+	flag = true;
+};
+inputid.onblur = function () {
+	boxid.style.boxShadow = "0 1px 1px 0 rgba(0, 0, 0, 0.16)";
+	flag = false;
+};
+boxid.onmouseover = function () {
+	boxid.style.boxShadow = "0 3px 6px 0 rgba(0, 0, 0, 0.16)";
+};
+boxid.onmouseout = function () {
+	if (!flag) boxid.style.boxShadow = "0 1px 1px 0 rgba(0, 0, 0, 0.16)";
+};
