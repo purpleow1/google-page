50caa0be3be8040ed492239a6f3005b6359e2862
purpleow1 <purpleowllight@gmail.com>
2018-01-13 00:36:56 +0300

    google-page/feature: add tooltips to search line and to search by voice icon

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 var flag = false;
 inputid.onfocus = function () {
-	boxid.style.boxShadow = "0 3px 6px 0 rgba(0, 0, 0, 0.16)";
+	searchboxid.style.boxShadow = "0 3px 6px 0 rgba(0, 0, 0, 0.16)";
 	flag = true;
 };
 inputid.onblur = function () {
-	boxid.style.boxShadow = "0 1px 1px 0 rgba(0, 0, 0, 0.16)";
+	searchboxid.style.boxShadow = "0 1px 1px 0 rgba(0, 0, 0, 0.16)";
 	flag = false;
 };
-boxid.onmouseover = function () {
-	boxid.style.boxShadow = "0 3px 6px 0 rgba(0, 0, 0, 0.16)";
+searchboxid.onmouseover = function () {
+	searchboxid.style.boxShadow = "0 3px 6px 0 rgba(0, 0, 0, 0.16)";
 };
-boxid.onmouseout = function () {
-	if (!flag) boxid.style.boxShadow = "0 1px 1px 0 rgba(0, 0, 0, 0.16)";
+searchboxid.onmouseout = function () {
+	if (!flag) searchboxid.style.boxShadow = "0 1px 1px 0 rgba(0, 0, 0, 0.16)";
 };
+
+voiceid.onmouseover = function () {
+	tooltip.style.visibility = "visible";
+}
+voiceid.onmouseout = function () {
+	tooltip.style.visibility = "hidden";
+}
