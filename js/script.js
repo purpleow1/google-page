bfa984d3e652f88c5756cf6da2b695af09cc7a52
purpleow1 <purpleowllight@gmail.com>
2018-01-12 20:50:16 +0300

    google-page/feature: add changing of search box shadow

---
+++ js/script.js
@@ -0,0 +1,6 @@
+inputid.onclick = function () {
+        boxid.style.boxShadow = "0 3px 6px 0 rgba(0, 0, 0, 0.16)";
+      };
+      inputid.onblur = function () {
+        boxid.style.boxShadow = "0 1px 1px 0 rgba(0, 0, 0, 0.16)";
+      };
