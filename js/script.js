54693c420051b24df884cc3b59d1f36361c92795
purpleow1 <purpleowllight@gmail.com>
2018-01-13 03:51:07 +0300

    google-page/fix: change About reference, make rafactoring of js code

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 var flag = false;
-inputid.onfocus = function () {
-	searchboxid.style.boxShadow = "0 3px 6px 0 rgba(0, 0, 0, 0.16)";
+document.getElementById("inputid").onfocus = function () {
+	document.getElementById("searchboxid").style.boxShadow = "0 3px 6px 0 rgba(0, 0, 0, 0.16)";
 	flag = true;
 };
-inputid.onblur = function () {
-	searchboxid.style.boxShadow = "0 1px 1px 0 rgba(0, 0, 0, 0.16)";
+document.getElementById("inputid").onblur = function () {
+	document.getElementById("searchboxid").style.boxShadow = "0 1px 1px 0 rgba(0, 0, 0, 0.16)";
 	flag = false;
 };
-searchboxid.onmouseover = function () {
-	searchboxid.style.boxShadow = "0 3px 6px 0 rgba(0, 0, 0, 0.16)";
+document.getElementById("searchboxid").onmouseover = function () {
+	document.getElementById("searchboxid").style.boxShadow = "0 3px 6px 0 rgba(0, 0, 0, 0.16)";
 };
-searchboxid.onmouseout = function () {
-	if (!flag) searchboxid.style.boxShadow = "0 1px 1px 0 rgba(0, 0, 0, 0.16)";
+document.getElementById("searchboxid").onmouseout = function () {
+	if (!flag) document.getElementById("searchboxid").style.boxShadow = "0 1px 1px 0 rgba(0, 0, 0, 0.16)";
 };
-
-voiceid.onmouseover = function () {
-	tooltip.style.visibility = "visible";
+document.getElementById("voiceid").onmouseover = function () {
+	document.getElementById("tooltipid").style.visibility = "visible";
 }
-voiceid.onmouseout = function () {
-	tooltip.style.visibility = "hidden";
+document.getElementById("voiceid").onmouseout = function () {
+	document.getElementById("tooltipid").style.visibility = "hidden";
 }
